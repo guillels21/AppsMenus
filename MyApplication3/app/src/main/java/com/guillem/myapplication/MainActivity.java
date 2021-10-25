b65d929711e4cede22a794b1e506b224a84e0497
Guillem Luque <guillem.luque@gmail.com>
2021-10-25 18:42:44 +0200

    second commit

--- MyApplication3/app/src/main/java/com/guillem/myapplication/MainActivity.java
+++ MyApplication3/app/src/main/java/com/guillem/myapplication/MainActivity.java
@@ -13,14 +13,27 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
 
+import com.google.android.material.snackbar.Snackbar;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.PopupMenu;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    Button button;
     Button boton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button);
+
 
         boton = (Button) findViewById(R.id.btnRight);
         boton.setOnClickListener(new View.OnClickListener() {
@@ -30,8 +43,23 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Creating the instance of PopupMenu
+                PopupMenu popup = new PopupMenu(MainActivity.this, button);
+                popup.getMenuInflater().inflate(R.menu.popup_menu, popup.getMenu());
+                popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    public boolean onMenuItemClick(MenuItem item) {
+                        Toast.makeText(MainActivity.this,"You Clicked : " + item.getTitle(), Toast.LENGTH_SHORT).show();
+                        return true;
+                    }
+                });
+                popup.show();
+            }
 
-    }
+        });
 
 
-    }
+    };
+}
